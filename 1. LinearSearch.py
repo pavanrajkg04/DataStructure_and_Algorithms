4a493d99628a35987594e72932a07df73e7d3025
Pavan Raj K G <nesarastudio0492@gmail.com>
2024-11-25 15:42:42 +0000

    one

---
+++ 1. LinearSearch.py
@@ -0,0 +1,16 @@
+def LinearSearch(list,Target):
+    for i in range(0,len(list)):
+        if list[i] == Target:
+            return i
+    return None
+
+def validate(result):
+    if result is not None:
+        print("The Target is : ",result)
+    else:
+        print("The target not found",result)
+
+
+
+result = LinearSearch([1,2,3,4,5,6,7,8,9,10],3)
+validate(result)
